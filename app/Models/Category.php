06c5848d3c05f4bb401d316b46e7b9cc2dddb204
刘浩宇 <2083709400@qq.com>
2019-05-30 17:15:04 +0800

    分类渲染

---
+++ app/Models/Category.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * 教师表
+ * Created by PhpStorm.
+ * User: cy
+ * Date: 2019/5/28
+ * Time: 18:35
+ */
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use DB;
+class Category extends Model
+{
+    protected $table = 'article_c';
+    public $timestamps = false;
+    public $primaryKey ="cid";
+   
+    public function ArticleList(){
+        $data = $this->get();
+        $article_arr = [];
+        foreach($data as $k => $v){
+            $article_arr[$k][] = $v;
+            $arr  = DB::table("article")->where("cid",$v->cid)->limit(4)->get();
+
+            if(!empty( $arr)){
+                foreach($arr as $key => $val){
+                    $val->content = $this->subtext(strip_tags($val->content),40);
+                    $val->add_time = date("m月d日",$val->add_time);
+                    $article_arr[$k][] = $val;
+                }
+            }
+            
+        }
+
+        return $article_arr;
+    }
+    public function subtext($text, $length)
+    {
+        if(mb_strlen($text, 'utf8') > $length) {
+            return mb_substr($text, 0, $length, 'utf8').'...';
+        } else {
+            return $text;
+        }
+    
+    }
+ 
+}
