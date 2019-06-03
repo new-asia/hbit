a13c11010ddc0e30e72e8f418dd1c5fddc077d8b
刘浩宇 <2083709400@qq.com>
2019-06-03 14:18:50 +0800

    标签

--- app/Http/Controllers/Home/IndexController.php
+++ app/Http/Controllers/Home/IndexController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use App\Models\Teacher;
 use App\Models\Student;
+use App\Models\Article;
+
 use App\Models\Advert;
 use App\Models\Course;
 use App\Models\Tags;
@@ -19,27 +21,28 @@ class IndexController extends Controller
         view()->composer('home.layouts.header','App\Http\Controllers\Home\NavController@header_nav');
     }
     public function index(){
+        
+        $isMobile = $this->isMobile();
+
+        if ($isMobile) {
+            $student = new Student();
+            $student_list = $student->getStudentSelect(4,2);
+            
+            $Article = new Article();
+            $Article_list = $Article->getArticleSelect();
+
+
+            return view('api/index',['student_list'=>$student_list,'article_list'=>$Article_list]);
+        }
+
         $advert = new Advert();
         $rotation_chart = $advert->rotation_chart();
+
         $course = new Course();
         $course = $course->cou_index();
-        $isMobile = $this->isMobile();
-        if ($isMobile) {
-            $employment = new Student();
-            $employment = $employment->Studentlist(4);
-            $employment = $employment->toArray();
-            foreach ($employment['data'] as $v){
-                $strlen     = mb_strlen($v->name, 'utf-8');
-                $firstStr     = mb_substr($v->name, 0, 1, 'utf-8');
-                $lastStr     = mb_substr($v->name, -1, 1, 'utf-8');
-                $v->name = $strlen == 2 ? $firstStr . str_repeat('*', mb_strlen($v->name, 'utf-8') - 1) : $firstStr . str_repeat("*", $strlen - 2) . $lastStr;
-                //echo $v->name;
-            }
-            //print_r($employment);die;
-            return view('api/index',['employment'=>$employment['data']]);
-        } else {
-            return view('home/index',['rotation_chart'=>$rotation_chart,'course'=>$course]);
-        }
+
+        return view('home/index',['rotation_chart'=>$rotation_chart,'course'=>$course]);
+        
         
     }
 
--- app/Models/Student.php
+++ app/Models/Student.php
@@ -31,7 +31,34 @@ class Student extends Model
             ->select('student_id', 'name','class_name','pay','testimonials','img')
             ->get();
     }
-
+    public function getStudentSelect($limit = 4,$sum = 1,$select = ""){
+        $sql = ['student_id','name','class_name','pay','img',"company"];
+        if(strstr($select,",")){
+            $sql = [ ];
+            $arr = explode($select,",");
+            foreach($arr as $v){
+                $sql[] = $v;
+            }
+        }
+        for($a = 1; $a <= $sum;$a++){
+            $str_arr = DB::table('student')->leftjoin('class','class.class_id','=','student.class_id')->where('is_graduate',1)
+            ->orderBy('list_order','desc')
+            ->skip(4 * ($a-1))
+            ->take($limit)
+            ->select($sql)
+            ->get()
+            ->toArray();
+            foreach ($str_arr as $v){
+                $strlen     = mb_strlen($v->name, 'utf-8');
+                $firstStr     = mb_substr($v->name, 0, 1, 'utf-8');
+                $lastStr     = mb_substr($v->name, -1, 1, 'utf-8');
+                $v->name = $strlen == 2 ? $firstStr . str_repeat('*', mb_strlen($v->name, 'utf-8') - 1) : $firstStr . str_repeat("*", $strlen - 2) . $lastStr;
+             }
+             $data[] = $str_arr;
+        }
+        
+        return $data;
+    }
     public function Studentlist($limit){
         return DB::table('student')
             ->leftjoin('class','class.class_id','=','student.class_id')
