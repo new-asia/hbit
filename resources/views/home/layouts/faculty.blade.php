6ca8a186f53460e8de27cb2ee94ff0441f67df95
刘浩宇 <2083709400@qq.com>
2019-06-04 00:49:46 +0800

    标签

--- resources/views/home/layouts/faculty.blade.php
+++ resources/views/home/layouts/faculty.blade.php
@@ -2,10 +2,10 @@
     @foreach($data as $v)
     <div style="width:210px; height:740px; float:left; margin-right:18.75px;">
         <div style=" height:410px;width:210px;">
-            <a href="/Faculty/show-3.html"><img src="{{$v->teacher_portrait_src}}" style="height:410px; width:210px;" height="410" width="210" /></a>
+            <a href="/Faculty/show-{{$v->teacher_id}}.html"><img src="{{$v->teacher_portrait_src}}" style="height:410px; width:210px;" height="410" width="210" /></a>
         </div>
         <div style="height:290px; width:188px; border:1px solid #F5F3F3;padding-left: 10px;padding-right: 10px;">
-            <a href="/Faculty/show-3.html"><div style="height:50px; line-height:50px; font-size:24px;">
+            <a href="/Faculty/show-{{$v->teacher_id}}.html"><div style="height:50px; line-height:50px; font-size:24px;">
                     {{$v->name}}
                 </div>
                 <div style="height:40px; line-height:40px; font-size:18px;">
